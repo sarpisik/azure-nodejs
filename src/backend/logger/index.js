3ca9589968110fea72bb2cabbffcf75dddd78b60
Sarp ISIK <sarp_isik91@hotmail.com>
2019-04-09 12:40:10 +0200

    edit logs path

--- src/backend/logger/index.js
+++ src/backend/logger/index.js
@@ -1,6 +1,6 @@
 const SimpleNodeLogger = require('simple-node-logger'),
   opts = {
-    logDirectory: 'logs',
+    logDirectory: '../../logs',
     fileNamePattern: 'LOGS-<DATE>.log',
     dateFormat: 'DD.MM.YYYY',
     timestampFormat: 'HH:mm:ss.SSS'
